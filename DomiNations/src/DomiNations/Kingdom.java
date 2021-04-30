0ece898f56c00c2b665a3726c099c2c0c5151628
Axel M <axelmannessiez@yahoo.fr>
2021-04-30 12:15:33 +0200

    Constructeurs Kingdom et Position

--- DomiNations/src/DomiNations/Kingdom.java
+++ DomiNations/src/DomiNations/Kingdom.java
@@ -2,16 +2,22 @@ package DomiNations;
 
 public class Kingdom {
 	private int size;
-	private final Player player = null;
-	
+	private final Player player;
+
+
+	public Kingdom(int size, Player player) {
+		this.size = size;
+		this.player = player;
+	}
+
 	public void setSize(int size) {
 		this.size = size;
 	}
-	
+
 	public int getSize() {
 		return size;
 	}
-	
+
 	public Player getPlayer() {
 		return player;
 	}
