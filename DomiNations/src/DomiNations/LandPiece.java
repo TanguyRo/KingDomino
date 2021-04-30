b6b4dc77e3dbfca7a2ce092798294598c7105e17
Axel M <axelmannessiez@yahoo.fr>
2021-04-30 10:10:41 +0200

    Classe LandPiece

--- DomiNations/src/DomiNations/LandPiece.java
+++ DomiNations/src/DomiNations/LandPiece.java
@@ -1,5 +1,29 @@
 package DomiNations;
 
 public class LandPiece {
-    
+    private Position position;
+    private final String type;
+    private final int crownNumber;
+
+    public LandPiece(String type, int crownNumber) {
+        this.position = null;
+        this.type = type;
+        this.crownNumber = crownNumber;
+    }
+
+    public void setCurrentPosition(Position position) {
+        this.position = position;
+    }
+
+    public Position getCurrentPosition() {
+        return position;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCrownNumber() {
+        return crownNumber;
+    }
 }
