74cfd02d68be9bbc04cad95d31dc9b459a021ac3
lou025 <wrallday@gmail.com>
2021-04-30 10:54:51 +0200

    Classe Kingdom
    setSize
    getSize
    getPlayer

--- DomiNations/src/DomiNations/Kingdom.java
+++ DomiNations/src/DomiNations/Kingdom.java
@@ -1,5 +1,18 @@
 package DomiNations;
 
 public class Kingdom {
-    
+	private int size;
+	private final Player player = null;
+	
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public Player getPlayer() {
+		return player;
+	}
 }
