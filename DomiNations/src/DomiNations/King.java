1ce0202beee5929b1e7cec0e09f92dadecffd4db
lou025 <wrallday@gmail.com>
2021-04-30 10:25:15 +0200

    Classe King
    setPlayer
    getPlayer

--- DomiNations/src/DomiNations/King.java
+++ DomiNations/src/DomiNations/King.java
@@ -1,5 +1,13 @@
 package DomiNations;
 
 public class King {
-    
+	public Player player;
+	
+	public void setPlayer(Player player){
+		this.player = player;
+	}
+	
+	public Player getPlayer(){
+		return player;
+	}
 }
