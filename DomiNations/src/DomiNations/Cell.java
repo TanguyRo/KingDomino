a1fb7d74a5a344751bc9004945aa586d6913cb11
Axel M <axelmannessiez@yahoo.fr>
2021-04-30 12:09:51 +0200

    Classe Cell

--- DomiNations/src/DomiNations/Cell.java
+++ DomiNations/src/DomiNations/Cell.java
@@ -1,5 +1,33 @@
 package DomiNations;
 
 public class Cell {
-    
+    private final Position position;
+    private boolean isEmpty;
+    private LandPiece currentLandPiece;
+
+    public Cell(Position position, boolean isEmpty, LandPiece currentLandPiece) {
+        this.position = position;
+        this.isEmpty = isEmpty;
+        this.currentLandPiece = currentLandPiece;
+    }
+
+    public void setState(boolean empty) {
+        isEmpty = empty;
+    }
+
+    public void setCurrentLandPiece(LandPiece currentLandPiece) {
+        this.currentLandPiece = currentLandPiece;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    public LandPiece getCurrentLandPiece() {
+        return currentLandPiece;
+    }
 }
