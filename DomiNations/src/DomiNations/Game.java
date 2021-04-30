e3867d8fd242d39dc5fe654ff641566d76baea07
Tanguy Robilliard <tanguy.robilliard@gmail.com>
2021-04-30 10:00:47 +0200

    game update

--- DomiNations/src/DomiNations/Game.java
+++ DomiNations/src/DomiNations/Game.java
@@ -1,5 +1,14 @@
 package DomiNations;
 
 public class Game {
+    private Player[] players;
+    private Player currentPlayer;
+    private Domino[] drawPile;
+
+    public static void createPlayers();
+    public static void initialiseBoards();
+    public static void initialiseDrawPile();
+    public static void play();
     
+
 }
