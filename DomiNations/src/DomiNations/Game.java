0151c082e1aaea89a408b66c7cfbd53302c857e8
Axel M <axelmannessiez@yahoo.fr>
2021-04-30 11:49:59 +0200

    fonction initialiseDrawPile

--- DomiNations/src/DomiNations/Game.java
+++ DomiNations/src/DomiNations/Game.java
@@ -1,16 +1,25 @@
 package DomiNations;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Game {
 	private Player[] players;
 	private Player currentPlayer;
-	private Domino[] drawPile;
+	private LinkedList<Domino> drawPile;
 	private int nbPlayers;
 
 	public void play() {
-		createPlayers();
+		//createPlayers();
 		initialiseBoards();
+		try {
+			initialiseDrawPile();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void createPlayers() {
@@ -19,6 +28,7 @@ public class Game {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Saisir nombre de joueur : ");
 		nbPlayers= scanner.nextInt();
+		scanner.nextLine();
 		for(int i = 1; i<nbPlayers; i++) {
 			System.out.println("Saisir nom joueur " + i + " : ");
 			String name = scanner.nextLine();
@@ -58,7 +68,38 @@ public class Game {
 		
 	}
 
-	public void initialiseDominos(){}
+	public void initialiseDrawPile() throws FileNotFoundException {
+		// scanner va lire le contenu du fichier .csv
+		Scanner scanner = new Scanner(new File("dominos.csv"));
+		scanner.nextLine();		// On saute la ligne d'en-tête
 
-    public void initialiseDrawPile(){}
+		// stringBuilder va stocker le contenu du fichier
+		StringBuilder stringBuilder = new StringBuilder();
+		while (scanner.hasNextLine()) {
+			stringBuilder.append(scanner.nextLine())
+					.append("\n");
+		}
+		scanner.close();
+
+		// Les deux lignes suivantes vont ensuite produire un String[] contenant
+		// les donneees du fichier CSV
+		String data = stringBuilder.toString();
+		String[] dominos = data.split("\n");
+
+		// Création de la pile de dominos
+		LinkedList<Domino> drawPile = new LinkedList<>();
+
+		// Création de tous les dominos et placement dans l'ordre dans la pile drawPile
+		for (int i=0; i < dominos.length; i++) {
+			String[] infosDomino = dominos[i].split(",");
+			LandPiece landPiece1 = new LandPiece(infosDomino[1],Integer.valueOf(infosDomino[0]));
+			LandPiece landPiece2 = new LandPiece(infosDomino[3],Integer.valueOf(infosDomino[2]));
+		    Domino domino = new Domino(landPiece1, landPiece2, Integer.valueOf(infosDomino[4]));
+			drawPile.add(domino);
+		}
+
+		// Mélange des dominos
+		Collections.shuffle(drawPile);
+	}
+
 }
