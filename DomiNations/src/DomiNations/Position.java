fb5f7b2ad15a5ed7e6e35c589aa773d4ab8d9a49
lou025 <wrallday@gmail.com>
2021-04-30 10:54:25 +0200

    Classe Position
    getColumn
    getRow

--- DomiNations/src/DomiNations/Position.java
+++ DomiNations/src/DomiNations/Position.java
@@ -1,5 +1,14 @@
 package DomiNations;
 
 public class Position {
-    
+	private final int column = 0;
+	private final int row = 0;
+	
+	public int getColumn() {
+		return column;
+	}
+	
+	public int getRow() {
+		return row;
+	}
 }
