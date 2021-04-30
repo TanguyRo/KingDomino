c07ab43fc0db0772e80828990595d6062de2a34b
lou025 <wrallday@gmail.com>
2021-04-30 11:09:43 +0200

    Classe Bench
    setFirstLane
    setSecondLane
    getFirstLane
    getSecondLane

--- DomiNations/src/DomiNations/Bench.java
+++ DomiNations/src/DomiNations/Bench.java
@@ -1,5 +1,22 @@
 package DomiNations;
 
 public class Bench {
-    
+	private Domino[] firstLane;
+	private Domino[] secondLane;
+	
+	public void setFirstLane(Domino[] firstLane){
+		this.firstLane = firstLane;
+	}
+	
+	public void setSecondLane(Domino[] firstLane){
+		this.secondLane = secondLane;
+	}
+	
+	public Domino[] getFirstLane(){
+		return firstLane;
+	}
+	
+	public Domino[] getSecondLane(){
+		return secondLane;
+	}
 }
