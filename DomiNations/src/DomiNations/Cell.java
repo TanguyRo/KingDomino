59a08b957fb401c86d3f605386e5e12dbba7a2e3
lou025 <wrallday@gmail.com>
2021-04-30 12:11:35 +0200

    Classe Cell
    setState
    setCurrentLandPiece
    getPosition
    getState
    getCurrentLandPiece

--- DomiNations/src/DomiNations/Cell.java
+++ DomiNations/src/DomiNations/Cell.java
@@ -1,5 +1,29 @@
 package DomiNations;
 
 public class Cell {
+    private final Position position;
+    private boolean isEmpty;
+    private LandPiece currentLandPiece;
     
+    public void setState(boolean isEmpty) {
+    	this.isEmpty = isEmpty;
+    }
+    
+    public void setCurrentLandPiece(LandPiece currentLandPiece) {
+    	this.currentLandPiece = currentLandPiece;
+    	
+    }
+    
+    public Position getPosition() {
+    	return Position;
+    }
+    
+    public boolean getState() {
+    	return isEmpty;
+ 
+    }
+    
+    public LandPiece getCurrentLandPiece() {
+    	return currentLandPiece;
+    }
 }
