201cc05d30c018dd65a17bec3d46a512e3ae0937
AxMannessiez <74686075+AxMannessiez@users.noreply.github.com>
2021-04-30 09:49:27 +0200

    Classes

---
+++ DomiNations/src/DomiNations/Main.java
@@ -0,0 +1,8 @@
+package DomiNations;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ DomiNations/src/DomiNations/Position.java
@@ -0,0 +1,5 @@
+package DomiNations;
+
+public class Position {
+    
+}
