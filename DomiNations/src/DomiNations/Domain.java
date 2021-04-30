1220e6334fa6aeb4bf506551350800a442f7ec3e
lou025 <wrallday@gmail.com>
2021-04-30 11:03:17 +0200

    Classe Domain
    setCrownNumber
    getCrownNumber
    getKingdom
    getType

--- DomiNations/src/DomiNations/Domain.java
+++ DomiNations/src/DomiNations/Domain.java
@@ -1,5 +1,23 @@
 package DomiNations;
 
 public class Domain {
-    
+	private final Kingdom kingdom = new Kingdom();
+	private final String type = "";
+	private int totalCrownNumber;
+	
+	public void setCrownNumber(int totalCrownNumber) {
+		this.totalCrownNumber = totalCrownNumber;
+	}
+	
+	public int getCrownNumber() {
+		return totalCrownNumber;
+	}
+	
+	public Kingdom getKingdom() {
+		return kingdom;
+	}
+	
+	public String getType() {
+		return type;
+	}
 }
