258269e80385b5e41ee01c7c63463ea996e9ce9b
lou025 <wrallday@gmail.com>
2021-05-07 10:29:28 +0200

    Update Game.java
    updated stream

--- DomiNations/src/DomiNations/Game.java
+++ DomiNations/src/DomiNations/Game.java
@@ -23,16 +23,6 @@ public class Game {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-<<<<<<< Updated upstream
-		
-		//Tour par Tour
-		
-		//(dans init drawpile ?)
-		//retire 24 dominos al�atoire si deux joueurs (24 restant dans la pioche)
-		//retire 12 dominos (36 restant dans la pioche) si 3 joueurs
-		//4 joueur : tout les dominos seront utilis�s
-=======
->>>>>>> Stashed changes
 			
 		//Premier Tour
 		//Piocher autant de dominos qu'il y a de rois en jeu
@@ -165,21 +155,10 @@ public class Game {
 
 		switch(nbPlayers) {
 			case 2:
-<<<<<<< Updated upstream
-				for(int i=1; i<24; i++) {
-					drawPile.remove();
-				}
-				break;
-			case 3:
-				for(int i=1; i<12; i++) {
-					drawPile.remove();
-				}
-=======
 				for(int i=1; i<24; i++) { drawPile.remove(); }
 				break;
 			case 3:
 				for(int i=1; i<12; i++) { drawPile.remove(); }
->>>>>>> Stashed changes
 				break;
 		}
 		
