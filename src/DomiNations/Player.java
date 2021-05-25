52f38994a629fd1170e0a487b567c190e1a096a3
Axel M <axelmannessiez@yahoo.fr>
2021-05-25 23:28:42 +0200

    Fonctions player.choosePosition()
    Pour demander au joueur les coordonnées et l'orientation de son domino, et de redemander si une information est incorrecte
    La version test permet de donner rapidement les 3 chiffres

--- src/DomiNations/Player.java
+++ src/DomiNations/Player.java
@@ -1,5 +1,8 @@
 package DomiNations;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Player {
     private final String name;
     private final Color color;
@@ -46,7 +49,132 @@ public class Player {
     }
 
     public int[] choosePosition() {
-        // TODO Scanner + vérifications chiffres entre 1 et 4 pour l'orientation et entre 0 et 4 pour la upLeftPosition
-        return new int[]{0, 1, 2};
+        Scanner scanner = new Scanner(System.in);
+
+        // Choix de l'orientation
+        int orientation = 0;
+            // Premier essai
+        try {
+            System.out.println("Saisir l'orientation du domino (1, 2, 3 ou 4) : ");
+            System.out.println("(1 correpond au domino horizontal tel quel, 2 correpond au domino pivoté de 90° dans le sens des aiguilles d'une montre, 3 au domino pivoté de 180° et 4 au domino pivoté de -90°)");
+            orientation = scanner.nextInt();
+            scanner.nextLine();
+            if (orientation<1 || orientation>4) {
+                orientation = 0;
+            }
+        }
+        catch (Exception ex) {
+            scanner.nextLine();
+            orientation = 0;
+        }
+            // Si ça n'a pas suffi on recommence jusqu'à avoir un chiffre correct
+        while (orientation == 0){
+            try {
+                System.out.println("L'orientation du domino ne peut être que 1, 2, 3 ou 4 : ");
+                orientation = scanner.nextInt();
+                scanner.nextLine();
+                if (orientation<1 || orientation>4) {
+                    orientation = 0;
+                }
+            }
+            catch (Exception ex) {
+                scanner.nextLine();
+                orientation = 0;
+            }
+        }
+
+        // Choix de la UpLeftPosition : coordonnée x
+        int UpLeftPositionX = 0;
+        // Premier essai
+        try {
+            System.out.println("Saisir la position du coin supérieur gauche de l'endroit ou vous souhaitez poser le domino.");
+            System.out.println("Colonne : (1 tout à gauche, 5 tout à droite)");
+            UpLeftPositionX = scanner.nextInt();
+            scanner.nextLine();
+            if (UpLeftPositionX<1 || UpLeftPositionX>5) {
+                UpLeftPositionX = 0;
+            }
+        }
+        catch (Exception ex) {
+            scanner.nextLine();
+            UpLeftPositionX = 0;
+        }
+        // Si ça n'a pas suffi on recommence jusqu'à avoir un chiffre correct
+        while (UpLeftPositionX == 0){
+            try {
+                System.out.println("La colonne ne peut être que 1, 2, 3, 4 ou 5 : ");
+                UpLeftPositionX = scanner.nextInt();
+                scanner.nextLine();
+                if (UpLeftPositionX<1 || UpLeftPositionX>5) {
+                    UpLeftPositionX = 0;
+                }
+            }
+            catch (Exception ex) {
+                scanner.nextLine();
+                UpLeftPositionX = 0;
+            }
+        }
+        UpLeftPositionX--;      // On passe en coordonnées Java (0-4 au lieu de 1-5)
+
+        // Choix de la UpLeftPosition : coordonnée y
+        int UpLeftPositionY = 0;
+        // Premier essai
+        try {
+            System.out.println("Ligne : (1 tout en haut, 5 tout en bas)");
+            UpLeftPositionY = scanner.nextInt();
+            scanner.nextLine();
+            if (UpLeftPositionY<1 || UpLeftPositionY>5) {
+                UpLeftPositionY = 0;
+            }
+        }
+        catch (Exception ex) {
+            scanner.nextLine();
+            UpLeftPositionY = 0;
+        }
+        // Si ça n'a pas suffi on recommence jusqu'à avoir un chiffre correct
+        while (UpLeftPositionY == 0){
+            try {
+                System.out.println("La colonne ne peut être que 1, 2, 3, 4 ou 5 : ");
+                UpLeftPositionY = scanner.nextInt();
+                scanner.nextLine();
+                if (UpLeftPositionY<1 || UpLeftPositionY>5) {
+                    UpLeftPositionY = 0;
+                }
+            }
+            catch (Exception ex) {
+                scanner.nextLine();
+                UpLeftPositionY = 0;
+            }
+
+        }
+        UpLeftPositionY--;      // On passe en coordonnées Java (0-4 au lieu de 1-5)
+
+        return new int[]{orientation, UpLeftPositionX, UpLeftPositionY};
     }
+
+    public int[] choosePositionTests() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Orientation, UpLeftPositionX, UpLeftPositionY : ");
+        boolean correctInput = false;
+        int[] values = new int[3];
+        while (!correctInput) {
+            try {
+                String input = scanner.nextLine();
+                input = input.replaceAll("\n", "");  // On enlève le retour à la ligne final
+                String[] inputs = input.split(", ");   // On sépare les 3 valeurs
+                for (int i = 0; i < 3; i++) {                // Conversion en int
+                    values[i] = Integer.parseInt(inputs[i]);
+                }
+                if (values[0] >= 1 && values[0] <= 4 && values[1] >= 1 && values[1] <= 5 && values[2] >= 1 && values[2] <= 5) {
+                    correctInput = true;
+                }
+            } catch (Exception ex) {
+                System.out.println("Réessayer : ");
+            }
+        }
+        values[1]--;
+        values[2]--;
+        return values;
+    }
+
 }
