aa1919187115adaa80f16795609423cca2e39821
Tanguy Robilliard <tanguy.robilliard@gmail.com>
2021-05-28 11:10:51 +0200

    interface update

--- src/DomiNations/Interface2test.java
+++ src/DomiNations/Interface2test.java
@@ -1,18 +1,22 @@
 package DomiNations;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Interface2test extends Application {
+    private int nbPlayers;
+
     public static void start() {
         launch(); // set up program as a javafx applicaton
     }
@@ -21,17 +25,11 @@ public class Interface2test extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+    //======================== Choix nombre joueurs =========================================
 
-        primaryStage.setTitle("Interface2test");
+        /*primaryStage.setTitle("Interface2test");
 
 //Scene 1
-        Label label1= new Label("Choix du nombre de joueurs");
-        Button buttonValider= new Button("Valider");
-        buttonValider.setOnAction(e -> primaryStage.setScene(scene2));
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, buttonValider);
-        scene1= new Scene(layout1, 300, 250);
-
         ToggleGroup group = new ToggleGroup();
         RadioButton button2 = new RadioButton("2");
         button2.setToggleGroup(group);
@@ -40,6 +38,15 @@ public class Interface2test extends Application {
         button3.setToggleGroup(group);
         RadioButton button4 = new RadioButton("4");
         button4.setToggleGroup(group);
+
+        Label label1= new Label("Choix du nombre de joueurs");
+        Button buttonValider= new Button("Valider");
+        buttonValider.setOnAction(e -> primaryStage.setScene(scene2));
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, buttonValider);
+        scene1= new Scene(layout1, 300, 250);
+
+
 
 //Scene 2
         Label label2= new Label("This is the second scene");
@@ -51,7 +58,71 @@ public class Interface2test extends Application {
 
 
         primaryStage.setScene(scene1);
-        primaryStage.show();
+        primaryStage.show();*/
+
+        primaryStage.setTitle("Déclaration des joueurs");
+        BorderPane borderPaneRoot = new BorderPane();                       //créer le groupe root border pane qui structure le contenu en 5 zones
+        Scene scene = new Scene(borderPaneRoot, 800, 600);           //créer le composant scene utilisant borderpane comme racine
+        primaryStage.setScene(scene);                                       //afficher la scene dans le stage
+
+
+        Stage otherStage = new Stage();
+        otherStage.setTitle("Nombre de joueurs");
+        otherStage.setWidth(400);
+        otherStage.setHeight(300);
+        otherStage.setResizable(false);
+        otherStage.show();
+        otherStage.centerOnScreen();
+        Label joueurs = new Label("Nombre de joueurs : ");
+        //group
+        ToggleGroup group = new ToggleGroup();
+
+        //Radio 1 : 2 joueurs : One
+        RadioButton button2 = new RadioButton("2");
+        button2.setToggleGroup(group);
+        button2.setSelected(true);
+        RadioButton button3 = new RadioButton("3");
+        button3.setToggleGroup(group);
+        RadioButton button4 = new RadioButton("4");
+        button4.setToggleGroup(group);
+
+        Button buttonValider = new Button("Valider");
+        HBox HBoxNbJoueurs = new HBox();
+        HBoxNbJoueurs.setPadding(new Insets(10));
+        HBoxNbJoueurs.setSpacing(5);
+        HBoxNbJoueurs.getChildren().addAll(joueurs, button2, button3, button4, buttonValider);
+
+        Scene sceneChoixNbJoueurs = new Scene(HBoxNbJoueurs, 400, 150);
+        otherStage.setScene(sceneChoixNbJoueurs);
+        otherStage.setTitle("Choix nombre de joueurs");
+        sceneChoixNbJoueurs.setRoot(HBoxNbJoueurs);
+        otherStage.show();
+
+
+        /*group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> ov, Toggle old_toggle, Toggle new_toggle) {
+                // Has selection.
+                if (group.getSelectedToggle() != null) {
+
+                    System.out.println("Button: " + button.getText());
+                    joueurs.setText("You are " + button.getText());
+
+                }
+            }
+        });*/
+        //lors de la validation du bouton chox enregiqtré
+        buttonValider.setOnAction(e->{
+            RadioButton buttonChoixNbPlayers = (RadioButton) group.getSelectedToggle();
+            nbPlayers = Integer.parseInt(buttonChoixNbPlayers.getText());                                       //ajouter le contenu de nom à la liste ListView
+            System.out.println(nbPlayers + " players selected");
+            otherStage.close();
+        });
+    // ========================================================================================
+
+
+
+
     }
 
     public static void main(String[] args) {
