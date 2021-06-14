588e2f40288426cbceb0ee8a97cca2cfec7105eb
Axel M <axelmannessiez@yahoo.fr>
2021-06-14 18:31:44 +0200

    Graphiques

--- src/DomiNations/JavaFxGameController.java
+++ src/DomiNations/JavaFxGameController.java
@@ -4,9 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
@@ -22,6 +25,16 @@ public class JavaFxGameController {
 
     @FXML
     private Button startButton;
+
+    // AnchorPane principal et différents fonds
+    @FXML
+    private AnchorPane mainAnchorPane;
+    @FXML
+    private ImageView twoPlayersBackground;
+    @FXML
+    private ImageView threePlayersBackground;
+    @FXML
+    private ImageView fourPlayersBackground;
 
     // 3 grandes tuiles
     @FXML
@@ -131,7 +144,23 @@ public class JavaFxGameController {
     }
 
     private void setPlayersParametersOnInterface(){
-        for (int i = 0; i < players.length; i++) {
+        // Fond
+        switch (nbPlayers) {
+            case 2 -> {
+                threePlayersBackground.setVisible(false);
+                fourPlayersBackground.setVisible(false);
+            }
+            case 3 -> {
+                twoPlayersBackground.setVisible(false);
+                fourPlayersBackground.setVisible(false);
+            }
+            default -> {
+                twoPlayersBackground.setVisible(false);
+                threePlayersBackground.setVisible(false);
+            }
+        }
+
+        for (int i = 0; i < nbPlayers; i++) {
             Player player = players[i];
             Color playerColor = player.getColor();
 
