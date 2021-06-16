4910be5dbc99461ee878699a8a0d441557b7fcd0
Axel M <axelmannessiez@yahoo.fr>
2021-06-16 23:19:08 +0200

    Renommage / nettoyage

--- src/DomiNations/Main.java
+++ src/DomiNations/Main.java
@@ -21,25 +21,6 @@ public class Main extends Application {
         stage.setTitle("DomiNations");
         stage.setScene(new Scene(root, 1280, 720));
         stage.show();
-
-        /*
-        // Initialisation du jeu
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/GameScene.fxml"));
-        Parent root = loader.load();
-        JavaFxGameController gameController = (JavaFxGameController) loader.getController();
-
-        gameStage.setTitle("DomiNations");
-        gameStage.setScene(new Scene(root, 1280, 720));
-        gameStage.show();
-
-
-         */
-
-        // Début du jeu : configuration du premier tour
-        // gameController.chooseDominosFirstRound();
-        //gameController.actualiseBench();
-        //System.out.println("Tous les dominos ont été sélectionnés par les joueurs. Celui avec la plus petite valeur commence en premier.");
-
     }
 
 
--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,4 @@
-module JavaFxGameTest {
+module DomiNations {
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
